c9452acd15e93795152cc838d92f2308c4326fac
Manender Kumar <manender369@gmail.com>
2021-09-16 07:21:34 -0400

    website updates

--- assets/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('#contact-success-message').hide();
-})
-
-$('#contact-us-form').submit(function(e) {
-  e.preventDefault();
-  $('#contact-success-message').show();
-})
